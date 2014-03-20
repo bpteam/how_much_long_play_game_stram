ceb37f9bab85b971c287224124e0cdb3fdf83df0
bpteam <bpteam22@gmail.com>
2014-03-21 00:34:42 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EC
+ * Date: 20.03.14
+ * Time: 23:06
+ * Project: how_much_long
+ * @author: Evgeny Pynykh bpteam22@gmail.com
+ */
+?>
+<?if(!isset($_POST['steam_link'])){?>
+	<form action="" method="POST">
+	Your steam link: <input type="text" name="steam_link">
+	<input type="submit" value="count"><br/>
+	Ex: http://steamcommunity.com/id/xxxKNxxx/games?tab=all
+</form>
+<?} elseif(preg_match('%^\s*(?<url>(http://)?(?<link>steamcommunity.com/id/(?<nic>[^/]+)/games\?tab=all))\s*$%ims', $_POST['steam_link'], $match)) {
+	$data = file_get_contents('http://'.$match['link']);
+	if(preg_match('%var\s*rgGames\s*=\s*(?<json>\[\{.*\}\]);%ims',$data,$match) && $json = json_decode($match['json'],true)){
+		$hours = 0;
+		foreach($json as $data){
+			if(isset($data['hours_forever'])){
+
+				$hours += str_replace(',','',$data['hours_forever']);
+			}
+		}
+		echo $hours;
+	} else {
+		echo 'Error parsing data';
+	}
+	?>
+<?
+} else {
+	echo 'WRONG LINK';
+}
